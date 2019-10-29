62c653d09a7d3ca23e695b1b4754e231eb4ef69d
Johann <johanson88@gmail.com>
2019-10-29 17:13:05 +0100

    Finished iteration 3

--- js/index.js
+++ js/index.js
@@ -3,15 +3,26 @@ function deleteItem(event){
 }
 
 function getPriceByProduct(itemNode){
-
+  return itemNode.value;
 }
 
 function updatePriceByProduct(productPrice, index){
-
+  let newPrice = document.querySelector('p.'+index );  
+  productPrice = parseFloat(productPrice.split('$').join(''));
+  let quantity = document.querySelector('input.'+index).value;
+  newPrice.innerHTML = parseFloat(productPrice) * parseInt(quantity) + '$'; 
 }
 
 function getTotalPrice() {
-
+  let arrayPrices = document.querySelectorAll('.price');
+  let totalPrice = 0;
+  arrayPrices.forEach(price => {
+    let priceClean = parseFloat(price.innerHTML.split('$').join(''));
+    totalPrice += priceClean;
+  });
+  console.log(totalPrice);
+  document.getElementById('total-price').innerHTML = totalPrice + ' $';
+  
 }
 
 function createQuantityInput(){
@@ -43,10 +54,24 @@ window.onload = function(){
   var createItemButton = document.getElementById('new-item-create');
   var deleteButtons = document.getElementsByClassName('btn-delete');
 
-  calculatePriceButton.onclick = getTotalPrice;
+  var inputFields = document.getElementsByTagName('input');
+  var inputsArray = [...inputFields];
+
+  inputsArray.forEach(input => input.addEventListener('change', (e) => {
+    let index = (e.currentTarget.className);
+    let price = document.querySelector('span.'+index ).innerHTML;
+    updatePriceByProduct(price,index);
+  }));
+
+  /*calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
 
   for(var i = 0; i<deleteButtons.length ; i++){
     deleteButtons[i].onclick = deleteItem;
   }
+  */
+
+
+
+  calculatePriceButton.addEventListener('click', getTotalPrice);
 };
